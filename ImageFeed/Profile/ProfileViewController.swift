2cedea3742d821106bd8e85e12880f57575dc802
Bakgeldi Alkhabay <bakgeldia@gmail.com>
2024-06-07 17:42:35 +0500

    Сверстан экран профиля пользователя ProfileViewController в Main.storyboard согласно дизайну макета.

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Bakgeldi Alkhabay on 07.06.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    
+    @IBAction private func didTapLogoutButton() {
+        
+    }
+}
