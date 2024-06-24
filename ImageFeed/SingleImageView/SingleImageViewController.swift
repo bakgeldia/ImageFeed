9e001e9f33e97888d07af8016b611d65585d7515
Bakgeldi Alkhabay <bakgeldia@gmail.com>
2024-06-24 20:20:15 +0500

    Добавлен SingleImageViewController

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Bakgeldi Alkhabay on 24.06.2024.
+//
+
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
